5a214fd58a7afef12500ad95f646849189cea9f7
heriawan <heriawan@aplog.co>
2025-08-04 18:25:34 +0700

    target build adj

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.aksara.notes"
         minSdk = 31
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
